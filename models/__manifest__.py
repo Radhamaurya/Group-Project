c4d70ef70b90ec2df32f983398fc8befdb46814f
krishnaKP1210 <krishnapanchal983@gmail.com>
2022-01-22 17:26:19 +0530

    Basic folders

---
+++ models/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'appointment',
+    'application': True,
+    'installble':True,
+    'data': [
+        'security/ir.model.access.csv',
+        ],
+    'license':'LGPL-3',
+}
---
+++ models/appointment.py
@@ -0,0 +1,4 @@
+from odoo import fields, models, api
+
+class Appointment_System(models.Model):
+    _name = 'appointment.system'
