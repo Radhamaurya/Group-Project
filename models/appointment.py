3fe828e69b00f02ed552f97b427c13bfca2f15dc
Radha-rama <rama@odoo.com>
2022-02-08 15:20:38 +0530

    Update Model And View

--- models/appointment.py
+++ models/appointment.py
@@ -5,7 +5,15 @@ from odoo import fields, models, api
 class Appointment_System(models.Model):
     _name = 'appointment.system'
 
+    name = fields.Char()
+    appointment_type = fields.Char()
     custmer = fields.Many2one("res.partner")
-    group = fields.Char()
-    timesheet = fields.Date()
+    time = fields.Date()
     price = fields.Float()
+    phone_number = fields.Integer()
+    # time_zone = fields.Timezone()
+    address = fields.Char()
+    location = fields.Char()
+    schedule_appointment = fields.Char()
+    appointment_duration = fields.Integer()
+     
